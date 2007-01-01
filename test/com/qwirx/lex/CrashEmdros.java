435d14ee4a7f0f58727a52b2d4a19c360527c74f
chris <chris@7faadb50-d407-0410-ad2f-ba9894573cc4>
2007-01-01 14:44:14 +0000

    - added another crash demo, when calling EMdFValue.getString() on an enumeration column
    git-svn-id: http://10.43.0.1/svn/lex/trunk@41 7faadb50-d407-0410-ad2f-ba9894573cc4

--- test/com/qwirx/lex/CrashEmdros.java
+++ test/com/qwirx/lex/CrashEmdros.java
@@ -7,10 +7,14 @@
 package com.qwirx.lex;
 
 import jemdros.EmdrosEnv;
-import jemdros.Table;
+import jemdros.MatchedObject;
+import jemdros.Sheaf;
+import jemdros.SheafConstIterator;
+import jemdros.Straw;
 import jemdros.TableRow;
 import jemdros.eCharsets;
 import jemdros.eOutputKind;
+import junit.framework.TestCase;
 
 /**
  * @author chris
@@ -18,9 +22,11 @@ import jemdros.eOutputKind;
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-public class CrashEmdros /* extends TestCase */ {
-
-	public void testCrash() {
+public class CrashEmdros extends TestCase 
+{
+    /*
+	public void testCrashWithZeroColumn() 
+    {
 		System.load("/usr/local/lib/emdros/libjemdros.so");
 		
 		EmdrosEnv env = new EmdrosEnv(eOutputKind.kOKConsole, 
@@ -37,10 +43,52 @@ public class CrashEmdros /* extends TestCase */ {
 		TableRow tr = t.iterator().next();
 		tr.getColumn(0);
 	}
-	
-	public static void main(String[] args) {
+
+    public void testCrashWithColumnTooLarge() 
+    {
+        System.load("/usr/local/lib/emdros/libjemdros.so");
+        
+        EmdrosEnv env = new EmdrosEnv(eOutputKind.kOKConsole, 
+                eCharsets.kCSISO_8859_1, "localhost", "emdf", 
+                "changeme", "wihebrew1202");
+
+        String query = "GET FEATURES argument_name " +
+                "FROM OBJECT WITH ID_D = 69977 [phrase]";
+
+        boolean[] bCompilerResult = new boolean[1];
+        env.executeString(query, bCompilerResult, false, false);
+        
+        Table t = env.takeOverTable();
+        TableRow tr = t.iterator().next();
+        tr.getColumn(3);
+    }
+    */
+
+    public void testCrashWithGetStringOnFeature() 
+    {
+        System.load("/usr/local/lib/emdros/libjemdros.so");
+        
+        EmdrosEnv env = new EmdrosEnv(eOutputKind.kOKConsole, 
+                eCharsets.kCSISO_8859_1, "localhost", "emdf", 
+                "changeme", "wihebrew1202");
+
+        String query = "SELECT ALL OBJECTS IN {1-28735} " +
+                "WHERE [phrase self = 69977 GET phrase_type]";
+
+        boolean[] bCompilerResult = new boolean[1];
+        env.executeString(query, bCompilerResult, false, false);
+        
+        Sheaf sheaf = env.takeOverSheaf();
+        SheafConstIterator sci = sheaf.const_iterator();
+        Straw straw = sci.next();
+        MatchedObject mo = straw.const_iterator().next();
+        String str = mo.getEMdFValue("phrase_type").getString();
+    }
+
+	public static void main(String[] args) 
+    {
 		// junit.textui.TestRunner.run(CrashEmdros.class);
-		new CrashEmdros().testCrash();
+		new CrashEmdros().testCrashWithGetStringOnFeature();
 	}
 
 }
