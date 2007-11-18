0fc1ede79c9bd536dd9dc93b894e4e52df6af2c8
chris <chris@7faadb50-d407-0410-ad2f-ba9894573cc4>
2007-11-18 17:04:11 +0000

    Remove some cases of throwing SQLException in public API.
    Copy stack trace from any SQLException into the DatabaseException.
    
    Add helpful messages when DatabaseException is thrown, particularly when conditions are invalid SQL.
    
    git-svn-id: http://10.43.0.1/svn/lex/trunk@153 7faadb50-d407-0410-ad2f-ba9894573cc4

--- src/com/qwirx/db/sql/SqlChange.java
+++ src/com/qwirx/db/sql/SqlChange.java
@@ -68,7 +68,8 @@ public final class SqlChange implements Change {
 		DELETE = new Type("DELETE");
 	
 	private int insertRowChangeLog(int originalRowId) 
-	throws SQLException {
+	throws SQLException
+	{
 		PreparedStatement stmt = prepareAndLogError(
 				"INSERT INTO changed_rows SET Log_ID = ?, " +
 				"Unique_ID = ?");
@@ -89,7 +90,8 @@ public final class SqlChange implements Change {
 	}			
 
 	private int findRowChangeLog(int originalRowId) 
-	throws SQLException {
+	throws SQLException
+	{
 		PreparedStatement stmt = prepareAndLogError(
 				"SELECT ID FROM changed_rows WHERE Log_ID = ? AND " +
 				"Unique_ID = ?");
@@ -105,7 +107,8 @@ public final class SqlChange implements Change {
 	}			
 
 	private void captureValues(boolean createRowChangeLogs, boolean storeAsNewValue) 
-	throws SQLException {
+	throws DatabaseException
+	{
 		/*
 		System.out.println("Capturing values for "+type.toString()+" ("+
 				createRowChangeLogs+", "+storeAsNewValue+")");
@@ -113,81 +116,153 @@ public final class SqlChange implements Change {
 		
 		StringBuffer sb = new StringBuffer("SELECT * FROM ").append(table);
 		
-		if (conditions != null) {
+		if (conditions != null)
+		{
 			sb.append(" WHERE ").append(conditions);
 		}
 		
-		PreparedStatement stmt = prepareAndLogError(sb.toString());
+		PreparedStatement stmt;
+		ResultSet rs;
 		
-		ResultSet         rs   = executeQueryAndLogError(stmt);
-		ResultSetMetaData md   = stmt.getMetaData();
+		try
+		{
+		    stmt = prepareAndLogError(sb.toString());
+		    rs = executeQueryAndLogError(stmt);
+		}
+		catch (SQLException e)
+		{
+		    throw new DatabaseException("Failed to capture old values " +
+		    		"of rows. You may have an error in your conditions: " +
+		    		conditions, e);
+		}
+
+		ResultSetMetaData md = null;
+		
+		try
+		{
+		    md = stmt.getMetaData();
+		}
+		catch (SQLException e)
+		{
+            throw new DatabaseException("Failed to get metadata for " +
+                "capturing old values of rows.", e);
+		}
 		
 		String destColName = storeAsNewValue ? "New_Value" :  "Old_Value";
 
-		PreparedStatement cvs = null;
-		if (createRowChangeLogs) {
-			cvs = prepareAndLogError(
-					"INSERT INTO changed_values SET "+destColName+" = ?, " +
-					"Row_ID = ?, Col_Name = ?");
-		} else {
-			cvs = prepareAndLogError(
-					"UPDATE changed_values SET "+destColName+" = ? " +
-					"WHERE Row_ID = ? AND Col_Name = ?");
-		}
+		try
+		{
+	        String query;
+
+	        if (createRowChangeLogs)
+	        {
+	            query = "INSERT INTO changed_values SET " + destColName +
+	                " = ?, Row_ID = ?, Col_Name = ?";
+	        }
+	        else
+	        {
+	            query = "UPDATE changed_values SET " + destColName + " = ? " +
+	                "WHERE Row_ID = ? AND Col_Name = ?";
+	        }
+
+	        PreparedStatement cvs = prepareAndLogError(query);
 		
-		while (rs.next()) {
-			int uniqueId = rs.getInt("ID");
-			
-			int logRowEntryId;
-			
-			if (createRowChangeLogs) {
-				logRowEntryId = insertRowChangeLog(uniqueId);
-			} else { 
-				logRowEntryId = findRowChangeLog(uniqueId);
-			}
-			
-			for (int i = 1; i <= md.getColumnCount(); i++) {
-				String colName  = md.getColumnName(i);
-				String curValue = rs.getString(i);
-				cvs.setString(1, curValue);
-				cvs.setInt   (2, logRowEntryId);
-				cvs.setString(3, colName);
-				cvs.executeUpdate();
-			}
+  			while (rs.next())
+  			{
+  			    int uniqueId = rs.getInt("ID");
 
-			stmt = conn.prepareStatement("DELETE FROM changed_values " +
-				"WHERE Row_ID = ? AND Old_Value = New_Value");
-			stmt.setInt(1, logRowEntryId);
-			stmt.executeUpdate();
-		}
+  			    int logRowEntryId;
 
-		cvs.close();
-		rs.close();
-		stmt.close();
+  			    if (createRowChangeLogs)
+  			    {
+  			        logRowEntryId = insertRowChangeLog(uniqueId);
+  			    }
+  			    else
+  			    { 
+  			        logRowEntryId = findRowChangeLog(uniqueId);
+  			    }
+
+  			    for (int i = 1; i <= md.getColumnCount(); i++)
+  			    {
+  			        String colName  = md.getColumnName(i);
+  			        String curValue = null;
+
+  			        // FIXME date 0000-00-00 in a MySQL database causes
+  			        // an exception when we call getString() on it
+  			        try
+  			        {
+  			            curValue = rs.getString(i);
+  			        }
+  			        catch (SQLException e)
+  			        {
+  			            if (e.getMessage().equals("Value '0000-00-00' " +
+  			                "can not be represented as java.sql.Date"))
+  			            {
+  			                // do nothing
+  			            }
+  			            else
+  			            {
+  			                throw e;
+  			            }
+  			        }
+
+  			        cvs.setString(1, curValue);
+  			        cvs.setInt   (2, logRowEntryId);
+  			        cvs.setString(3, colName);
+  			        cvs.executeUpdate();
+  			    }
+
+  			    stmt = prepareAndLogError("DELETE FROM changed_values " +
+  			        "WHERE Row_ID = ? AND ((Old_Value = New_Value) OR " +
+  			        "(Old_Value IS NULL AND New_Value IS NULL))");
+
+  			    stmt.setInt(1, logRowEntryId);
+  			    stmt.executeUpdate();
+  			}
+
+  			cvs.close();
+  			rs.close();
+  			stmt.close();
+        }
+        catch (SQLException e)
+        {
+            throw new DatabaseException("Failed to store old values in " +
+                "change tracking tables. Perhaps there is a problem " +
+                "with the tables?", e);
+        }
 	}
 	
 	private PreparedStatement prepareAndLogError(String query) 
-	throws SQLException {
-		try {
+	throws SQLException
+	{
+		try
+		{
 			return conn.prepareStatement(query);
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			System.err.println("Error preparing query ("+query+"): "+e);
 			throw(e);
 		}
 	}
 	
 	private ResultSet executeQueryAndLogError(PreparedStatement stmt) 
-	throws SQLException {
-		try {
+	throws SQLException 
+	{
+		try
+		{
 			return stmt.executeQuery();
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			System.err.println("Error executing query ("+stmt.toString()+"): "+e);
 			throw(e);
 		}
 	}
 	
 	private void captureOldValues() 
-	throws SQLException {
+	throws DatabaseException
+	{
 		if (type != UPDATE && type != DELETE)
 			throw new AssertionError("this method can only be used "+
 					"when the command type is UPDATE or DELETE");
@@ -196,25 +271,38 @@ public final class SqlChange implements Change {
 	}
 	
 	private void captureNewValues() 
-	throws SQLException 
+	throws DatabaseException 
 	{
 		if (type != INSERT && type != UPDATE)
+		{
 			throw new AssertionError("this method can only be used "+
 					"when the command type is INSERT or UPDATE");
+		}
 		
-		if (type == INSERT) {
-			PreparedStatement stmt = prepareAndLogError(
-					"SELECT LAST_INSERT_ID()");
-			ResultSet rs = executeQueryAndLogError(stmt);
-			rs.next();
-			insertedRowId = rs.getInt(1);
-			rs.close();
-			stmt.close();
-			
+		if (type == INSERT)
+		{
+		    try
+		    {
+    			PreparedStatement stmt = prepareAndLogError(
+    					"SELECT LAST_INSERT_ID()");
+    			ResultSet rs = executeQueryAndLogError(stmt);
+    			rs.next();
+    			insertedRowId = rs.getInt(1);
+    			rs.close();
+    			stmt.close();
+		    }
+		    catch (SQLException e)
+		    {
+		        throw new DatabaseException("Failed to get the ID " +
+		        		"of the last inserted row", e);
+		    }
+		    
 			conditions = "ID = " + insertedRowId;
 
 			captureValues(true, true);
-		} else {
+		}
+		else
+		{
 			captureValues(false, true);
 		}
 	}
@@ -222,9 +310,53 @@ public final class SqlChange implements Change {
 	public void execute() throws DatabaseException 
     {
         long startTime = System.currentTimeMillis();
-		StringBuffer sb = new StringBuffer("Query not prepared yet");
+		StringBuffer sb = new StringBuffer();
+        
+        if (type == INSERT) 
+        {
+            sb.append("INSERT INTO ").append(table);
+        }
+        else if (type == UPDATE)
+        {
+            sb.append("UPDATE ").append(table);
+        }
+        else if (type == DELETE)
+        {
+            sb.append("DELETE FROM ").append(table);
+        }
+        else
+        {
+            throw new AssertionError("type != INSERT && type != UPDATE "+
+                    "&& type != DELETE");
+        }
+        
+        if (type == INSERT || type == UPDATE) 
+        {
+            sb.append(" SET ");
+            for (Iterator keys = fields.keySet().iterator(); keys.hasNext(); ) 
+            {
+                String key = (String)( keys.next() );
+                Object value = fields.get(key);
+                
+                if (value instanceof Constant)
+                {
+                    sb.append(key+" = "+value);
+                }
+                else
+                {
+                    sb.append(key+" = ?");
+                }
+                
+                if (keys.hasNext()) sb.append(", ");
+            }
+        }
+        
+        if ((type == UPDATE || type == DELETE) && conditions != null) {
+            sb.append(" WHERE ").append(conditions);
+        }
 		
-		try {
+		try
+		{
 			boolean oldAutoCommit = conn.getAutoCommit();
 			conn.setAutoCommit(false);
 			
@@ -245,51 +377,15 @@ public final class SqlChange implements Change {
 			rs.close();
 			stmt.close();
 			
-			if (type == UPDATE || type == DELETE) {
+			if (type == UPDATE || type == DELETE)
+			{
 				captureOldValues();
-			}
-			
-			sb = new StringBuffer();
-			
-			if (type == INSERT) {
-				sb.append("INSERT INTO ").append(table);
-			} else if (type == UPDATE) {
-				sb.append("UPDATE ").append(table);
-			} else if (type == DELETE) {
-				sb.append("DELETE FROM ").append(table);
-			} else {
-				throw new AssertionError("type != INSERT && type != UPDATE "+
-						"&& type != DELETE");
-			}
-			
-			if (type == INSERT || type == UPDATE) 
-            {
-				sb.append(" SET ");
-				for (Iterator keys = fields.keySet().iterator(); keys.hasNext(); ) 
-                {
-                    String key = (String)( keys.next() );
-                    Object value = fields.get(key);
-                    
-                    if (value instanceof Constant)
-                    {
-                        sb.append(key+" = "+value);
-                    }
-                    else
-                    {
-                        sb.append(key+" = ?");
-                    }
-					
-                    if (keys.hasNext()) sb.append(", ");
-				}
-			}
-			
-			if ((type == UPDATE || type == DELETE) && conditions != null) {
-				sb.append(" WHERE ").append(conditions);
 			}
 			
 			stmt = prepareAndLogError(sb.toString());
 			
-			if (type == INSERT || type == UPDATE) {
+			if (type == INSERT || type == UPDATE)
+			{
 				int i = 0;
 				for (Iterator keys = fields.keySet().iterator(); keys.hasNext(); ) 
                 {
@@ -321,18 +417,24 @@ public final class SqlChange implements Change {
         catch (SQLException e) 
         {
             m_LOG.error(sb.toString(), e);
-			throw new DatabaseException(e, sb.toString());
+            DatabaseException de = new DatabaseException("Failed to execute " +
+                "the requested operation. Please check your field names " +
+                "and values", e, sb.toString());
+            de.setStackTrace(e.getStackTrace());
+			throw de;
 		}
         
         long totalTime = System.currentTimeMillis() - startTime;
         m_LOG.info(totalTime+" ms to track "+sb.toString());
 	}
 	
-	public void setInt(String column, long value) {
+	public void setInt(String column, long value)
+	{
 		fields.put(column, Long.toString(value));
 	}
 
-	public void setString(String column, String value) {
+	public void setString(String column, String value)
+	{
 		fields.put(column, value);
 	}
 
@@ -342,14 +444,18 @@ public final class SqlChange implements Change {
         // fields.put(column, new Constant(value));
 	}
 
-	public int getId() {
+	public int getId()
+	{
 		return this.id;
 	}
 	
-	public int getInsertedRowId() {
-		if (type != INSERT) 
+	public int getInsertedRowId()
+	{
+		if (type != INSERT)
+		{
 			throw new IllegalStateException(
-					"Only INSERT changes have an inserted ID");
+				"Only INSERT changes have an inserted ID");
+		}
 		return insertedRowId;
 	}
 }
