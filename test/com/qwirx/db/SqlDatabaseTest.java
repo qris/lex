16d2f95dd490e4d77d1d60fee02489eeb721a0af
chris <chris@7faadb50-d407-0410-ad2f-ba9894573cc4>
2007-11-19 08:24:42 +0000

    Test that date 0000-00-00 is tracked correctly.
    Test that DECIMAL columns are tracked correctly.
    
    Test that UPDATEs which change the found set are tracked correctly.
    
    git-svn-id: http://10.43.0.1/svn/lex/trunk@162 7faadb50-d407-0410-ad2f-ba9894573cc4

--- test/com/qwirx/db/SqlDatabaseTest.java
+++ test/com/qwirx/db/SqlDatabaseTest.java
@@ -24,7 +24,8 @@ import com.qwirx.db.sql.SqlDatabase;
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-public class SqlDatabaseTest extends TestCase {
+public class SqlDatabaseTest extends TestCase
+{
 	private SqlDatabase db;
 	
     public void testDatabasePreparedStatements() throws Exception
@@ -55,10 +56,14 @@ public class SqlDatabaseTest extends TestCase {
 		db = new SqlDatabase(conn, "test", "test");
 	}
 	
-	public void setUp() throws Exception {
-		try {
+	public void setUp() throws Exception
+	{
+		try
+		{
 			db.executeDirect("DROP TABLE logtest");
-		} catch (DatabaseException e) {
+		}
+		catch (DatabaseException e)
+		{
 			// ignore errors, we don't mind if table doesn't exist yet
 		}
 		
@@ -67,7 +72,7 @@ public class SqlDatabaseTest extends TestCase {
 				"t_int int4," +
 				"t_str varchar(40)," +
 				"t_txt mediumtext," + 
-				// "t_flt decimal(10,2)," + // not supported by JDBC?
+				"t_flt decimal(10,2)," + // not supported by JDBC?
 				"t_dat date, "+
 				"t_dtm datetime, "+
                 "t_dat2 DATE NOT NULL"+
@@ -83,7 +88,8 @@ public class SqlDatabaseTest extends TestCase {
 	private int    logRecordId;
 	
 	public void getChangeTypeAndId(int xaId) 
-	throws DatabaseException, SQLException {
+	throws DatabaseException, SQLException
+	{
 		PreparedStatement stmt = db.prepareSelect(
 			"SELECT ID, Cmd_Type FROM change_log WHERE ID = ?");
 		stmt.setInt(1, xaId);
@@ -95,12 +101,14 @@ public class SqlDatabaseTest extends TestCase {
 	}
 	
 	public void checkCurrentValues(String query, ChangedRow cr) 
-	throws DatabaseException, SQLException {
+	throws DatabaseException, SQLException
+	{
 		db.prepareSelect(query);
 		ResultSet rs = db.select();
 		rs.next();
 
-		for (Iterator i = cr.iterator(); i.hasNext(); ) {
+		for (Iterator i = cr.iterator(); i.hasNext();)
+		{
 			Object n = i.next();
 			assertEquals(ChangedValue.class, n.getClass());
 			ChangedValue cv = (ChangedValue)n;
@@ -116,7 +124,7 @@ public class SqlDatabaseTest extends TestCase {
                 if (e.getMessage().equals("Value '0000-00-00' " +
                         "can not be represented as java.sql.Date"))
                 {
-                    // do nothing
+                    curValue = "0000-00-00";
                 }
                 else
                 {
@@ -125,7 +133,8 @@ public class SqlDatabaseTest extends TestCase {
             }
 
             String expValue = cv.getNewValue();
-			assertEquals("Column has wrong value", expValue, curValue);
+			assertEquals("Column " + colName + " has wrong value",
+			    expValue, curValue);
 		}
 
 		db.finish();
@@ -139,21 +148,26 @@ public class SqlDatabaseTest extends TestCase {
 				"New_Value FROM changed_values WHERE Row_ID = " + rowLogId);
 		
 		ResultSet rs = db.select();
-		while (rs.next()) {
+		while (rs.next())
+		{
 			String colName  = rs.getString(2);
 			ChangedValue cv = cr.get(colName);
-			assertNotNull("Unknown row added to log: " 
-					+ colName, cv);
+			assertNotNull("Unknown row added to log: " + colName + 
+			    " changed from " + rs.getString(3) +
+				" to " + rs.getString(4), cv);
 			
 			{
 				String expOldValue = cv.getOldValue();
 				String curOldValue = rs.getString(3);
 				boolean wasNull = rs.wasNull();
-				if (expOldValue == null) {
+				if (expOldValue == null)
+				{
 					assertTrue("Old value was not NULL but " + curOldValue + 
 							" in changed_values row "+rs.getInt(1),
 							wasNull);
-				} else {
+				}
+				else
+				{
 					assertEquals("Wrong old value logged for "+colName, 
 							expOldValue, curOldValue);
 				}
@@ -180,26 +194,29 @@ public class SqlDatabaseTest extends TestCase {
 		db.finish();
 
 		Iterator i = cr.iterator();
-		if (i.hasNext()) {
+		if (i.hasNext())
+		{
 			ChangedValue cv = (ChangedValue)( i.next() );
 			assertTrue("Expected column change log not found: "+ 
 					cv.getName(), false);
 		}
 	}
 	
-	private void insertTestRecord() throws DatabaseException, SQLException {
-		Change ch = db.createChange(SqlChange.INSERT, 
+	private int insertTestRecord() throws DatabaseException, SQLException
+	{
+		SqlChange ch = (SqlChange)db.createChange(SqlChange.INSERT, 
 				"logtest", null);
 		ch.setString("t_int", "1234");
 		ch.setString("t_str", "Hello World");
 		ch.setString("t_txt", "A somewhat longer string");
-		// xa.setString("t_flt", "3.14159"); // will be truncated
+		ch.setString("t_flt", "3.14159"); // will be truncated
 		ch.setString("t_dat", "1979-01-07"); // happy birthday
 		ch.setString("t_dtm", "1979-01-07 06:35:00");
         ch.setString("t_dat2", "0000-00-00"); // crashes some versions
         // of mysql jdbc when getting column values
 		ch.execute();
 		getChangeTypeAndId(ch.getId());
+		return ch.getInsertedRowId();
 	}
 	
 	public void testInsert() throws Exception 
@@ -215,18 +232,21 @@ public class SqlDatabaseTest extends TestCase {
 		ResultSet rs = db.select();
 		rs.next();
 		int rowLogId = rs.getInt(1);
-		if (rs.next()) {
+		if (rs.next())
+		{
 			throw new AssertionError("too many rows added to changed_rows table");
 		}
 		db.finish();
 		
 		ChangedRow cr = new ChangedRow();
-		cr.put(new ChangedValue("ID",    null, "1"));
-		cr.put(new ChangedValue("t_int", null, "1234"));
-		cr.put(new ChangedValue("t_str", null, "Hello World"));
-		cr.put(new ChangedValue("t_txt", null, "A somewhat longer string"));
-		cr.put(new ChangedValue("t_dat", null, "1979-01-07")); // happy birthday
-		cr.put(new ChangedValue("t_dtm", null, "1979-01-07 06:35:00.0"));
+		cr.put(new ChangedValue("ID",     null, "1"));
+		cr.put(new ChangedValue("t_int",  null, "1234"));
+		cr.put(new ChangedValue("t_str",  null, "Hello World"));
+		cr.put(new ChangedValue("t_txt",  null, "A somewhat longer string"));
+        cr.put(new ChangedValue("t_flt",  null, "3.14"));
+		cr.put(new ChangedValue("t_dat",  null, "1979-01-07")); // happy birthday
+		cr.put(new ChangedValue("t_dtm",  null, "1979-01-07 06:35:00.0"));
+        cr.put(new ChangedValue("t_dat2", null, "0000-00-00"));
 		
 		checkCurrentValues("SELECT * FROM logtest", cr);
 		checkAndRemoveChanges(rowLogId, cr);
@@ -234,14 +254,14 @@ public class SqlDatabaseTest extends TestCase {
 	
 	public void testUpdate() throws Exception
 	{
-		insertTestRecord();
+		int testId = insertTestRecord();
 
 		Change ch = db.createChange(SqlChange.UPDATE, 
-				"logtest", null);
+				"logtest", "t_int = 1234");
 		ch.setInt   ("t_int", 23456);
 		ch.setString("t_str", "Hello Again");
 		ch.setString("t_txt", "Another longer string");
-		// xa.setString("t_flt", "3.14159"); // will be truncated
+		ch.setString("t_flt", "2.81718"); // will be truncated
 		ch.setString("t_dat", "1980-10-15"); // happy birthday
 		ch.setString("t_dtm", "1980-10-15 12:34:56.0");
 		ch.execute();
@@ -253,22 +273,23 @@ public class SqlDatabaseTest extends TestCase {
 				"SELECT ID FROM changed_rows " +
 				"WHERE Log_ID = ? AND Unique_ID = ?");
 		stmt.setInt(1, logRecordId);
-		stmt.setInt(2, 1); // unique ID of only row in "logtest" table.
+		stmt.setInt(2, testId);
 		ResultSet rs = db.select();
 		
 		int rowLogId;
 		
-		try {
+		try
+		{
 			rs.next();
 			rowLogId = rs.getInt(1);
-		} catch (SQLException e) {
+		}
+		catch (SQLException e) 
+		{
 			System.err.println(stmt.toString());
 			throw(e);
 		}
 		
-		if (rs.next()) {
-			throw new AssertionError("too many rows added to changed_rows table");
-		}
+		assertFalse("too many rows added to changed_rows table", rs.next());
 		db.finish();
 
 		ChangedRow cr = new ChangedRow();
@@ -281,6 +302,7 @@ public class SqlDatabaseTest extends TestCase {
 		cr.put(new ChangedValue("t_txt", 
 				"A somewhat longer string",
 				"Another longer string"));
+        cr.put(new ChangedValue("t_flt", "3.14", "2.82"));
 		cr.put(new ChangedValue("t_dat", 
 				"1979-01-07", "1980-10-15"));
 		cr.put(new ChangedValue("t_dtm", 
@@ -310,26 +332,32 @@ public class SqlDatabaseTest extends TestCase {
 		
 		int rowLogId;
 		
-		try {
+		try
+		{
 			rs.next();
 			rowLogId = rs.getInt(1);
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			System.err.println(stmt.toString());
 			throw(e);
 		}
 		
-		if (rs.next()) {
+		if (rs.next())
+		{
 			throw new AssertionError("too many rows added to changed_rows table");
 		}
 		db.finish();
 
 		ChangedRow cr = new ChangedRow();
-		cr.put(new ChangedValue("ID",    "1",	                     null));
-		cr.put(new ChangedValue("t_int", "1234",	                 null));
-		cr.put(new ChangedValue("t_str", "Hello World",              null));
-		cr.put(new ChangedValue("t_txt", "A somewhat longer string", null));
-		cr.put(new ChangedValue("t_dat", "1979-01-07",               null));
-		cr.put(new ChangedValue("t_dtm", "1979-01-07 06:35:00.0",    null));
+		cr.put(new ChangedValue("ID",     "1",	                      null));
+		cr.put(new ChangedValue("t_int",  "1234",	                  null));
+		cr.put(new ChangedValue("t_str",  "Hello World",              null));
+		cr.put(new ChangedValue("t_txt",  "A somewhat longer string", null));
+        cr.put(new ChangedValue("t_flt",  "3.14",                     null));
+		cr.put(new ChangedValue("t_dat",  "1979-01-07",               null));
+		cr.put(new ChangedValue("t_dtm",  "1979-01-07 06:35:00.0",    null));
+        cr.put(new ChangedValue("t_dat2", "0000-00-00",               null));
 
 		stmt = db.prepareSelect("SELECT * FROM logtest");
 		rs = stmt.executeQuery();
@@ -337,6 +365,66 @@ public class SqlDatabaseTest extends TestCase {
 		db.finish();
 		
 		checkAndRemoveChanges(rowLogId, cr);
+	}
+
+	public void testUpdateWhichChangesFoundSet() throws Exception
+	{
+	    insertTestRecord();
+
+	    Change ch = db.createChange(SqlChange.UPDATE, 
+	        "logtest", null);
+	    ch.setInt   ("t_int", 23456);
+	    ch.setString("t_str", "Hello Again");
+	    ch.setString("t_txt", "Another longer string");
+	    ch.setString("t_flt", "2.81718"); // will be truncated
+	    ch.setString("t_dat", "1980-10-15"); // happy birthday
+	    ch.setString("t_dtm", "1980-10-15 12:34:56.0");
+	    ch.execute();
+
+	    getChangeTypeAndId(ch.getId());
+	    assertEquals("UPDATE", changeType);
+
+	    PreparedStatement stmt = db.prepareSelect(
+	        "SELECT ID FROM changed_rows " +
+	    "WHERE Log_ID = ? AND Unique_ID = ?");
+	    stmt.setInt(1, logRecordId);
+	    stmt.setInt(2, 1); // unique ID of only row in "logtest" table.
+	    ResultSet rs = db.select();
+
+	    int rowLogId;
+
+	    try
+	    {
+	        rs.next();
+	        rowLogId = rs.getInt(1);
+	    }
+	    catch (SQLException e) 
+	    {
+	        System.err.println(stmt.toString());
+	        throw(e);
+	    }
+
+	    assertFalse("too many rows added to changed_rows table", rs.next());
+	    db.finish();
+
+	    ChangedRow cr = new ChangedRow();
+	    cr.put(new ChangedValue("t_int", 
+	        "1234",                     
+	    "23456"));
+	    cr.put(new ChangedValue("t_str", 
+	        "Hello World",
+	    "Hello Again"));
+	    cr.put(new ChangedValue("t_txt", 
+	        "A somewhat longer string",
+	    "Another longer string"));
+	    cr.put(new ChangedValue("t_flt", "3.14", "2.82"));
+	    cr.put(new ChangedValue("t_dat", 
+	        "1979-01-07", "1980-10-15"));
+	    cr.put(new ChangedValue("t_dtm", 
+	        "1979-01-07 06:35:00.0", "1980-10-15 12:34:56.0"));
+
+	    checkCurrentValues("SELECT * FROM logtest", cr);
+	    checkAndRemoveChanges(rowLogId, cr);
 	}
 
 	public static void main(String[] args) {
