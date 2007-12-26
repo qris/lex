bf61715e37da0182d72156e620eec0c6688a376a
chris <chris@7faadb50-d407-0410-ad2f-ba9894573cc4>
2007-12-26 20:31:04 +0000

    Replace methods needed for EmdrosDatabaseTest.
    git-svn-id: http://10.43.0.1/svn/lex/trunk@182 7faadb50-d407-0410-ad2f-ba9894573cc4

--- src/com/qwirx/db/ChangedRow.java
+++ src/com/qwirx/db/ChangedRow.java
@@ -6,8 +6,10 @@
  */
 package com.qwirx.db;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 
@@ -18,22 +20,42 @@ import java.util.Map;
  * Window - Preferences - Java - Code Style - Code Templates
  */
 public class ChangedRow {
-	private Map values = new Hashtable();
+	private Map m_Values = new Hashtable();
+
+    public ChangedRow() { }
+
+    public ChangedRow(ChangedValue[] values)
+    {
+        for (int i = 0; i < values.length; i++)
+        {
+            m_Values.put(values[i].getName(), values[i]);
+        }
+    }
 	
 	public void put(ChangedValue change) {
-		values.put(change.getName(), change);
+		m_Values.put(change.getName(), change);
 	}
 	
 	public ChangedValue get(String columnName) {
-		return (ChangedValue)( values.get(columnName) );
+		return (ChangedValue)( m_Values.get(columnName) );
 	}
+    
+    public List getColumns()
+    {
+        return new ArrayList(m_Values.keySet());
+    }
+    
+    public List getValues()
+    {
+        return new ArrayList(m_Values.values());
+    }
 	
 	public Iterator iterator() {
-		return values.values().iterator();
+		return m_Values.values().iterator();
 	}
 	
 	public void remove(String columnName) {
-		values.remove(columnName);
+		m_Values.remove(columnName);
 	}
 	
 	public ChangedRow reverse() {
