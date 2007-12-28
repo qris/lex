0dfe00b28e1b6cdcc77b6e2fda4b56309d7bda8f
chris <chris@7faadb50-d407-0410-ad2f-ba9894573cc4>
2007-12-28 00:54:12 +0000

    Add latest crash test cases.
    Update library loading code.
    
    git-svn-id: http://10.43.0.1/svn/lex/trunk@203 7faadb50-d407-0410-ad2f-ba9894573cc4

--- test/com/qwirx/lex/CrashEmdros.java
+++ test/com/qwirx/lex/CrashEmdros.java
@@ -6,11 +6,16 @@
  */
 package com.qwirx.lex;
 
+import java.net.URL;
+
 import jemdros.EmdrosEnv;
 import jemdros.MatchedObject;
 import jemdros.Sheaf;
 import jemdros.SheafConstIterator;
 import jemdros.Straw;
+import jemdros.StrawConstIterator;
+import jemdros.Table;
+import jemdros.TableRow;
 import jemdros.eCharsets;
 import jemdros.eOutputKind;
 import junit.framework.TestCase;
@@ -23,10 +28,54 @@ import junit.framework.TestCase;
  */
 public class CrashEmdros extends TestCase 
 {
-    /*
-	public void testCrashWithZeroColumn() 
+    private boolean m_LibraryLoaded = false;
+    
+    private void loadLibrary()
     {
-		System.load("/usr/local/lib/emdros/libjemdros.so");
+        if (m_LibraryLoaded)
+        {
+            return;
+        }
+        
+        URL url = Preloader.class.getResource("/../lib/");
+        
+        if (url == null)
+        {
+            url = Preloader.class.getResource("/../jsp/WEB-INF/lib/");
+        }
+        
+        if (url == null)
+        {
+            url = Preloader.class.getResource("/");
+        }
+
+        String path = url.getPath() + "libjemdros.so";
+        if (! new java.io.File(path).exists())
+        {
+            path = "/usr/local/lib/emdros/libjemdros.so";
+        }
+        if (! new java.io.File(path).exists())
+        {
+            path = "/home/chris/tomcat/common/lib/libjemdros.so";
+        }
+        
+        if (new java.io.File(path).exists())
+        {
+            System.out.println("Library path: "+path);
+            System.load(path);
+        }
+        else
+        {
+            System.out.println("Library not found, trying System.loadLibrary()");
+            System.loadLibrary("libjemdros.so");
+        }
+        
+        m_LibraryLoaded = true;
+    }
+
+	public void testCrashWithZeroColumn() throws Exception
+    {
+		loadLibrary();
 		
 		EmdrosEnv env = new EmdrosEnv(eOutputKind.kOKConsole, 
 				eCharsets.kCSISO_8859_1, "localhost", "emdf", 
@@ -43,9 +92,9 @@ public class CrashEmdros extends TestCase
 		tr.getColumn(0);
 	}
 
-    public void testCrashWithColumnTooLarge() 
+    public void testCrashWithColumnTooLarge() throws Exception
     {
-        System.load("/usr/local/lib/emdros/libjemdros.so");
+        loadLibrary();
         
         EmdrosEnv env = new EmdrosEnv(eOutputKind.kOKConsole, 
                 eCharsets.kCSISO_8859_1, "localhost", "emdf", 
@@ -61,16 +110,15 @@ public class CrashEmdros extends TestCase
         TableRow tr = t.iterator().next();
         tr.getColumn(3);
     }
-    */
 
-    public void testCrashWithGetStringOnFeature() 
+    public void testCrashJavaWithGetStringOnFeature1() 
     throws Exception
     {
-        System.load("/usr/local/lib/emdros/libjemdros.so");
+        loadLibrary();
         
         EmdrosEnv env = new EmdrosEnv(eOutputKind.kOKConsole, 
                 eCharsets.kCSISO_8859_1, "localhost", "emdf", 
-                "changeme", "wihebrew1202");
+                "changeme", "wihebrew");
 
         String query = "SELECT ALL OBJECTS IN {1-28735} " +
                 "WHERE [phrase self = 69977 GET phrase_type]";
@@ -84,11 +132,89 @@ public class CrashEmdros extends TestCase
         MatchedObject mo = straw.const_iterator().next();
         String str = mo.getEMdFValue("phrase_type").getString();
     }
+    
+    public void testAbortJavaWithSheafConstIteratorNext() throws Exception
+    {
+        loadLibrary();
+        
+        EmdrosEnv env = new EmdrosEnv(eOutputKind.kOKConsole, 
+            eCharsets.kCSISO_8859_1, "localhost", "emdf", "changeme", 
+            "wihebrew");
+
+        boolean[] bCompilerResult = new boolean[1];
+
+        assertTrue(env.executeString(
+            "SELECT ALL OBJECTS IN " +
+            "{1-1000000} " +
+            "WHERE " +
+            "[verse GET book, chapter, verse " +
+            " [clause self = 1 " +
+            "  [word GET text, graphical_word, lexeme]"+
+            " ]"+
+            "]", bCompilerResult, false, false));
+
+        Sheaf sheaf = env.takeOverSheaf();
+        
+        SheafConstIterator sci = sheaf.const_iterator();
+        Straw straw = sci.next();
+        /*
+        StrawConstIterator swci = straw.const_iterator();
+        MatchedObject verse = swci.next();
+        
+        verse.getEMdFValue("book").getString();
+        verse.getEMdFValue("chapter").getInt();
+        verse.getEMdFValue("verse").getInt();
+
+        sci = verse.getSheaf().const_iterator();
+        straw = sci.next();
+        swci = straw.const_iterator();
+        MatchedObject clause = swci.next();
+        */
+    }
+
+    public void testCrashJavaWithGetStringOnFeature2() throws Exception
+    {
+        loadLibrary();
+
+        EmdrosEnv env = new EmdrosEnv(eOutputKind.kOKConsole, 
+            eCharsets.kCSISO_8859_1, "localhost", "emdf", "changeme", 
+            "wihebrew");
+
+        boolean[] bCompilerResult = new boolean[1];
+
+        assertTrue(env.executeString(
+            "SELECT ALL OBJECTS IN " +
+            "{1-1000000} " +
+            "WHERE " +
+            "[verse GET book, chapter, verse " +
+            " [clause self = 1324989 " +
+            "  [word GET text, graphical_word, lexeme]"+
+            " ]"+
+            "]", bCompilerResult, false, false));
+
+        Sheaf sheaf = env.takeOverSheaf();
+
+        SheafConstIterator sci = sheaf.const_iterator();
+        Straw straw = sci.next();
+        StrawConstIterator swci = straw.const_iterator();
+        MatchedObject verse = swci.next();
+        
+        verse.getEMdFValue("book").getString();
+        /*
+        verse.getEMdFValue("chapter").getInt();
+        verse.getEMdFValue("verse").getInt(); 
+
+        sci = verse.getSheaf().const_iterator();
+        straw = sci.next();
+        swci = straw.const_iterator();
+        MatchedObject clause = swci.next();
+        */
+    }
 
 	public static void main(String[] args) throws Exception
     {
 		// junit.textui.TestRunner.run(CrashEmdros.class);
-		new CrashEmdros().testCrashWithGetStringOnFeature();
+		new CrashEmdros().testCrashJavaWithGetStringOnFeature1();
 	}
 
 }
