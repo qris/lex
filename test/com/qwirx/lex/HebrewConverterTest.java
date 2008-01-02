9248c27564490b2f0360522d49376516aab22b94
chris <chris@7faadb50-d407-0410-ad2f-ba9894573cc4>
2008-01-02 20:58:52 +0000

    More transliteration test cases.
    git-svn-id: http://10.43.0.1/svn/lex/trunk@252 7faadb50-d407-0410-ad2f-ba9894573cc4

--- test/com/qwirx/lex/HebrewConverterTest.java
+++ test/com/qwirx/lex/HebrewConverterTest.java
@@ -9,6 +9,9 @@ public class HebrewConverterTest extends TestCase
     public void testToTranslit()
     {
         assertEquals("nākərîî", HebrewConverter.toTranslit("N@K:RIJ."));
+        assertEquals("jiśśāskār", HebrewConverter.toTranslit("JIF.@#K@R"));
+        assertEquals("rē?šî", HebrewConverter.toTranslit("R;>CI73J"));
+        assertEquals("îm", HebrewConverter.toTranslit("I92Jm"));
     }
 
     public static void main(String[] args)
