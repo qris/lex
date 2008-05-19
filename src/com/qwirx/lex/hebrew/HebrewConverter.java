3dd8c2f205a9e75aecef08063ded7842cea260ee
chris <chris@7faadb50-d407-0410-ad2f-ba9894573cc4>
2008-05-19 19:22:45 +0000

    Add $ character to output a B as a hack for LexemeTest.
    git-svn-id: http://10.43.0.1/svn/lex/trunk@287 7faadb50-d407-0410-ad2f-ba9894573cc4

--- src/com/qwirx/lex/hebrew/HebrewConverter.java
+++ src/com/qwirx/lex/hebrew/HebrewConverter.java
@@ -370,6 +370,7 @@ public class HebrewConverter
                 output.append(output.charAt(output.length() - 1));
             }
             else if (c == ',') { } // Ignore Rafe, cf. 2SA 11,01 mmale?kim
+            else if (c == '$') { output.append("b"); } // hack for LexemeTest
 
             else
             {
