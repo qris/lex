f1e3c66e187f660a8b20038d72a27168acb6b33a
chris <chris@7faadb50-d407-0410-ad2f-ba9894573cc4>
2008-06-22 19:15:49 +0000

    Clause class factored out of clause.jsp.
    git-svn-id: http://10.43.0.1/svn/lex/trunk@297 7faadb50-d407-0410-ad2f-ba9894573cc4

---
+++ test/com/qwirx/lex/Clause.java
@@ -0,0 +1,252 @@
+package com.qwirx.lex;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import jemdros.EmdrosException;
+import jemdros.MatchedObject;
+import jemdros.Sheaf;
+import jemdros.SheafConstIterator;
+import jemdros.Straw;
+
+import com.qwirx.db.DatabaseException;
+import com.qwirx.db.sql.SqlDatabase;
+import com.qwirx.lex.emdros.EmdrosDatabase;
+import com.qwirx.lex.hebrew.HebrewConverter;
+import com.qwirx.lex.lexicon.Lexeme;
+import com.qwirx.lex.morph.HebrewMorphemeGenerator;
+
+public class Clause
+{
+    private EmdrosDatabase m_Emdros;
+    private SqlDatabase    m_Lexicon;
+    private MatchedObject  m_Clause;
+    
+    private Clause(EmdrosDatabase emdros, SqlDatabase lexicon,
+        MatchedObject clause)
+    {
+        m_Emdros  = emdros;
+        m_Lexicon = lexicon;
+        m_Clause  = clause;
+    }
+    
+    public static Clause find(EmdrosDatabase emdros, SqlDatabase lexicon,
+        int id)
+    throws DatabaseException
+    {
+        String query = 
+            "SELECT ALL OBJECTS IN " +
+            emdros.getVisibleMonads() +
+            " WHERE [clause self = "+id+
+            "       GET logical_struct_id, logical_structure "+
+            "        [phrase GET phrase_type, phrase_function, argument_name, "+
+            "                    type_id, macrorole_number "+
+            "          [word GET lexeme, phrase_dependent_part_of_speech, " +
+            "                    tense, stem, wordnet_gloss, wordnet_synset, " +
+            "                    graphical_preformative, " +
+            "                    graphical_locative, " +
+            "                    graphical_lexeme, " +
+            "                    graphical_pron_suffix, " +
+            "                    graphical_verbal_ending, " +
+            "                    graphical_root_formation, " +
+            "                    graphical_nominal_ending, " +
+            "                    person, number, gender, state, " +
+            "                    surface_consonants, " +
+            "                    suffix_person, suffix_number, suffix_gender " +
+            "          ]"+
+            "        ]"+
+            "      ]";
+        
+        try
+        {
+            Sheaf sheaf = emdros.getSheaf(query);
+            Straw straw = sheaf.const_iterator().next();
+            MatchedObject clause = straw.const_iterator().next();
+            
+            return new Clause(emdros, lexicon, clause);
+        }
+        catch (EmdrosException e)
+        {
+            throw new DatabaseException("Failed to find clause " + id, e,
+                query);
+        }
+    }
+    
+    public int getLogicalStructureID()
+    throws DatabaseException
+    {
+        try
+        {
+            return m_Clause.getEMdFValue("logical_struct_id").getInt();
+        }
+        catch (EmdrosException e)
+        {
+            throw new DatabaseException("Failed to get logical structure ID",
+                e);
+        }
+    }
+    
+    public String getEvaluatedLogicalStructure()
+    throws DatabaseException
+    {
+        int lsid = getLogicalStructureID();
+        
+        Lexeme lexeme = Lexeme.load(m_Lexicon, lsid);
+        if (lexeme == null)
+        {
+            /*
+            throw new AssertionError("Logical structure not found " +
+                    "with ID = " + lsid + " for clause " + m_Clause.getID_D());
+            */
+            return null;
+        }
+        
+        String logicalStructure = lexeme.getLogicalStructure();
+        Set<String> argNames = new TreeSet<String>();
+
+        Pattern varPat = Pattern.compile
+            ("(?s)(?i)<([^>]*)>");
+        Matcher m = varPat.matcher(logicalStructure);
+        while (m.find()) 
+        {
+            String arg = m.group(1);
+            argNames.add(m.group(1));
+        }
+
+        Map<String, MatchedObject> variables = 
+            new HashMap<String, MatchedObject>();
+
+        SheafConstIterator phrases = m_Clause.getSheaf().const_iterator();
+        while (phrases.hasNext())
+        {
+            MatchedObject phrase = null;
+            
+            try
+            {
+                phrase = phrases.next().const_iterator().next();
+            }
+            catch (EmdrosException e)
+            {
+                throw new DatabaseException("Failed to get phrase", e);
+            }
+            
+            String argName = null;
+            
+            try
+            {
+                argName = phrase.getEMdFValue("argument_name").toString();
+            }
+            catch (EmdrosException e)
+            {
+                throw new DatabaseException("Failed to get argument name", e);
+            }
+
+            if (argName.equals("")) 
+            {
+                // Argument not decided yet. Maybe we can guess
+                // based on the "function" of the clause?
+    
+                String function_name = null;
+                
+                try
+                {
+                    function_name = phrase.getFeatureAsString(
+                        phrase.getEMdFValueIndex("phrase_function"));
+                }
+                catch (EmdrosException e)
+                {
+                    throw new DatabaseException("Failed to get phrase " +
+                            "function", e);
+                }
+
+                if (function_name.equals("Subj") ||
+                    function_name.equals("PreS") ||
+                    function_name.equals("IrpS") ||
+                    function_name.equals("ModS"))
+                {
+                    argName = "x";
+                }
+                else if (function_name.equals("Objc") ||
+                    function_name.equals("PreC") ||
+                    function_name.equals("PreO") ||
+                    function_name.equals("PtcO") ||
+                    function_name.equals("IrpO"))
+                {
+                    argName = "y";
+                }
+            }
+            
+            variables.put(argName, phrase);
+        }
+        
+        HebrewMorphemeGenerator hmg = new HebrewMorphemeGenerator();
+
+        for (Iterator<String> i = variables.keySet().iterator(); i.hasNext();) 
+        {
+            String variable = i.next();
+            MatchedObject phrase = variables.get(variable);
+            String value_text = "";
+            SheafConstIterator sci = phrase.getSheaf().const_iterator();
+            
+            while (sci.hasNext())
+            {
+                try
+                {
+                    MatchedObject word = sci.next().const_iterator().next();
+                    value_text += HebrewConverter.wordTranslitToHtml(word, hmg);
+                }
+                catch (EmdrosException e)
+                {
+                    throw new DatabaseException("Failed to transliterate word", 
+                        e);
+                }
+                
+                if (sci.hasNext())
+                {
+                    value_text += " ";
+                }
+            }
+            
+            logicalStructure = logicalStructure.replaceAll("<" +
+                variable + ">", value_text);
+        }
+        
+        return logicalStructure;
+    }
+
+    public String getPredicateText()
+    throws EmdrosException
+    {
+        SheafConstIterator phrases = m_Clause.getSheaf().const_iterator();
+
+        while (phrases.hasNext()) 
+        {
+            MatchedObject phrase =
+                phrases.next().const_iterator().next();
+
+            String function_name = phrase.getFeatureAsString(
+                phrase.getEMdFValueIndex("phrase_function"));
+
+            SheafConstIterator words = phrase.getSheaf().const_iterator();
+            while (words.hasNext()) 
+            {
+                MatchedObject word = words.next().const_iterator().next();
+
+                String psp = word.getFeatureAsString(
+                    word.getEMdFValueIndex("phrase_dependent_part_of_speech")); 
+                    
+                if (psp.equals("verb"))
+                {
+                    return word.getEMdFValue("lexeme").getString();
+                }
+            }
+        }
+        
+        return null;
+    }
+}
