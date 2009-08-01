0f5380ee379f60a03dcdb84e55e96f9c6224641e
chris <chris@7faadb50-d407-0410-ad2f-ba9894573cc4>
2009-08-01 20:19:21 +0000

    Move useful methods from ClauseControllerTest to LexTestBase, and derive from LexTestBase.
    git-svn-id: http://10.43.0.1/svn/lex/trunk@378 7faadb50-d407-0410-ad2f-ba9894573cc4

--- test/com/qwirx/lex/ClauseControllerTest.java
+++ test/com/qwirx/lex/ClauseControllerTest.java
@@ -30,47 +30,6 @@ public class ClauseControllerTest extends LexTestBase
 {
     public ClauseControllerTest() throws Exception { }
     
-    private Map<String, Integer> m_Location = new HashMap<String, Integer>();
-
-    private MatchedObject getNestedObject(Sheaf sheaf)
-    {
-        SheafConstIterator sci = sheaf.const_iterator();
-        
-        if (sci.hasNext()) 
-        {
-            Straw straw = sci.next();
-            StrawConstIterator swci = straw.const_iterator();
-            if (swci.hasNext()) 
-            {
-                return swci.next();
-            }
-        }
-
-        return null;
-    }
-    
-    private MatchedObject getWord(int monad)
-    throws Exception
-    {
-        HebrewMorphemeGenerator hmg = new HebrewMorphemeGenerator();
-        String query = "SELECT ALL OBJECTS IN {" + monad + "} " +
-            "WHERE [word GET " + hmg.getRequiredFeaturesString(true) + "]";
-        Sheaf sheaf = getEmdros().getSheaf(query);
-        MatchedObject word = getNestedObject(sheaf);
-        
-        Table table = getEmdros().getTable("SELECT OBJECTS HAVING MONADS IN {" + 
-            monad + "} [ALL]");
-        for (TableIterator ti = table.iterator(); ti.hasNext();)
-        {
-            TableRow row = ti.next();
-            String type = row.getColumn(1);
-            Integer id_d = Integer.valueOf(row.getColumn(3));
-            m_Location.put(type, Integer.valueOf(id_d));
-        }
-        
-        return word;
-    }
-    
     private void assertGlossCell(Cell cell, MatchedObject word)
     throws Exception
     {
--- test/com/qwirx/lex/LexTestBase.java
+++ test/com/qwirx/lex/LexTestBase.java
@@ -1,9 +1,23 @@
 package com.qwirx.lex;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import jemdros.EmdrosException;
+import jemdros.MatchedObject;
+import jemdros.Sheaf;
+import jemdros.SheafConstIterator;
+import jemdros.Straw;
+import jemdros.StrawConstIterator;
+import jemdros.Table;
+import jemdros.TableIterator;
+import jemdros.TableRow;
 import junit.framework.TestCase;
 
 import com.qwirx.db.sql.SqlDatabase;
 import com.qwirx.lex.emdros.EmdrosDatabase;
+import com.qwirx.lex.morph.HebrewMorphemeGenerator;
 
 public class LexTestBase extends TestCase
 {
@@ -18,4 +32,47 @@ public class LexTestBase extends TestCase
     
     protected SqlDatabase getSql() { return m_SQL; }
     protected EmdrosDatabase getEmdros() { return m_Emdros; }
+    
+    protected Map<String, Integer> m_Location = new HashMap<String, Integer>();
+
+    protected MatchedObject getNestedObject(Sheaf sheaf)
+    throws EmdrosException
+    {
+        SheafConstIterator sci = sheaf.const_iterator();
+        
+        if (sci.hasNext()) 
+        {
+            Straw straw = sci.next();
+            StrawConstIterator swci = straw.const_iterator();
+            if (swci.hasNext()) 
+            {
+                return swci.next();
+            }
+        }
+
+        return null;
+    }
+    
+    protected MatchedObject getWord(int monad)
+    throws Exception
+    {
+        HebrewMorphemeGenerator hmg = new HebrewMorphemeGenerator();
+        String query = "SELECT ALL OBJECTS IN {" + monad + "} " +
+            "WHERE [word GET " + hmg.getRequiredFeaturesString(true) + "]";
+
+        Sheaf sheaf = getEmdros().getSheaf(query);
+        MatchedObject word = getNestedObject(sheaf);
+        
+        Table table = getEmdros().getTable("SELECT OBJECTS HAVING MONADS IN {" + 
+            monad + "} [ALL]");
+        for (TableIterator ti = table.iterator(); ti.hasNext();)
+        {
+            TableRow row = ti.next();
+            String type = row.getColumn(1);
+            Integer id_d = Integer.valueOf(row.getColumn(3));
+            m_Location.put(type, Integer.valueOf(id_d));
+        }
+        
+        return word;
+    }
 }
