b6ec7ebcf7d27f1a38b8c6f637801b6f9cff78ae
chris <chris@7faadb50-d407-0410-ad2f-ba9894573cc4>
2010-01-14 13:28:27 +0000

    Add ability to disable database checks by creating a "com.qwirx.db.properties" file in the classpath and setting "database.check.enabled = no" in it (see haobackend for example).
    git-svn-id: http://10.43.0.1/svn/lex/trunk@400 7faadb50-d407-0410-ad2f-ba9894573cc4

--- src/com/qwirx/db/sql/DbTable.java
+++ src/com/qwirx/db/sql/DbTable.java
@@ -3,6 +3,10 @@
  */
 package com.qwirx.db.sql;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,20 +14,21 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
 public class DbTable 
 {
-	private String name, m_Charset;
-	private DbColumn [] columns;
+	private String m_Name, m_Charset;
+	private DbColumn [] m_Columns;
     private static final Logger m_LOG = Logger.getLogger(DbTable.class);
 
 	public DbTable(String name, String charset, DbColumn [] columns) 
     {
-		this.name    = name;
+		this.m_Name    = name;
         this.m_Charset = charset;
-		this.columns = columns;
+		this.m_Columns = columns;
 	}
 
     private void cleanUpSql(Statement stmt, ResultSet rs)
@@ -32,35 +37,101 @@ public class DbTable
 		try { stmt.close(); } catch (Exception e) { /* ignore */ }
     }
 
+    private static boolean DATABASE_CHECKS_ENABLED = true;
+    private static File DATABASE_CONFIG_FILE;
+	public static final String DATABASE_CHECK_PROPERTY = 
+		"database.check.enabled";
+    
+    static
+    {
+    	URL propFileBase = DbTable.class.getResource("/");
+    	String propFileName = "com.qwirx.db.properties";
+    	DATABASE_CONFIG_FILE = new File(propFileBase.getPath(), propFileName);
+    	
+    	if (!DATABASE_CONFIG_FILE.exists())
+    	{
+    		m_LOG.warn("Failed to load database properties file: " + 
+    				DATABASE_CONFIG_FILE);
+    	}
+    	else
+    	{
+    		Properties props = new Properties();
+    		try
+    		{
+    			props.load(new FileInputStream(DATABASE_CONFIG_FILE));
+    		}
+    		catch (IOException e)
+    		{
+        		m_LOG.warn("Failed to load database properties file: " + 
+        				DATABASE_CONFIG_FILE, e);		
+    		}
+    		
+    		m_LOG.info("Loading database configuration from " +
+    				DATABASE_CONFIG_FILE);
+
+    		String enabled = props.getProperty(DATABASE_CHECK_PROPERTY);
+    		
+    		if (enabled != null)
+    		{
+    			if (enabled.equalsIgnoreCase("no") ||
+    				enabled.equalsIgnoreCase("false"))
+    			{
+    				DATABASE_CHECKS_ENABLED = false;
+    	    		m_LOG.info("Database checks disabled: enable by setting " +
+    	    				DATABASE_CHECK_PROPERTY + "to 'yes'");
+    			}
+    		}
+
+    		if (DATABASE_CHECKS_ENABLED)
+    		{
+	    		m_LOG.info("Database checks enabled: disable by setting " +
+	    				DATABASE_CHECK_PROPERTY + "to 'no'");
+    		}
+    	}    	
+    }
+
     public void check(Connection conn, boolean addMissingColumns) 
     throws SQLException, IllegalStateException 
     {
+    	if (DATABASE_CHECKS_ENABLED)
+    	{
+			m_LOG.info("Checking table " + m_Name);
+    	}
+    	else
+    	{
+    		m_LOG.info("Not checking table " + m_Name);
+    		return;    		
+    	}
+
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		String sql = null;
         
 		try 
 		{
-			stmt = conn.prepareStatement
-				("SHOW TABLES");
+			stmt = conn.prepareStatement("SHOW TABLES");
 			rs = stmt.executeQuery();
 			
 			while (rs.next()) 
 			{
 				String tableName = rs.getString(1);
-				if (tableName.equals(name)) 
+				
+				if (tableName.equals(m_Name)) 
 				{	
 					cleanUpSql(stmt, rs);
-                    sql = "DESCRIBE " + name;
+                    sql = "DESCRIBE " + m_Name;
 					stmt = conn.prepareStatement(sql);
 					rs = stmt.executeQuery();
-					Iterator tci = Arrays.asList(columns)
+					Iterator tci = Arrays.asList(m_Columns)
 						.iterator();
-					while (rs.next()) {
+					
+					while (rs.next())
+					{
 						String currentColumnName = rs.getString(1);
-						if (!tci.hasNext()) {
+						if (!tci.hasNext())
+						{
 							throw new IllegalStateException
-								("Table " + name +
+								("Table " + m_Name +
 								" contains unknown column " + 
 								currentColumnName);
 						}
@@ -68,11 +139,13 @@ public class DbTable
 						DbColumn expectedCol = (DbColumn)(tci.next());
 						String expectedColName = expectedCol.name;
 						if (!expectedColName.equalsIgnoreCase(currentColumnName))
+						{
 							throw new IllegalStateException
-								("Table "+name+
+								("Table "+m_Name+
 								" column "+currentColumnName+
 								" should be called "+expectedColName);
-
+						}
+						
 						String expectedType = expectedCol.type.toLowerCase();
                         if (expectedType.startsWith("varchar"))
                         {
@@ -86,11 +159,13 @@ public class DbTable
                         }
                         
 						if (! actualType.equals(expectedType))
+						{
 							throw new IllegalStateException
-								("Table "+name+
+								("Table "+m_Name+
 								" column "+currentColumnName+
 								" should have type "+expectedType+
 								" instead of "+actualType);
+						}
 						
 						boolean expectedNull = expectedCol.isNullAllowed;
                         boolean passed = false; 
@@ -112,7 +187,7 @@ public class DbTable
 						if (! passed)
                         {
 							throw new IllegalStateException
-								("Table "+name+
+								("Table "+m_Name+
 								" column "+currentColumnName+
 								" should"+(expectedNull ? "" : " not")+
 								" allow NULL ("+actualNull+")");
@@ -122,22 +197,26 @@ public class DbTable
                     rs.close();
                     stmt.close();
 					
-                    stmt = conn.prepareStatement("SHOW CREATE TABLE " +
-                        this.name);
-                    rs = stmt.executeQuery();
-                    rs.next();
-                    String charset = rs.getString(2);
-                    rs.close();
-                    stmt.close();
-                    
-                    charset = charset.replaceFirst("(?s).* DEFAULT CHARSET=", "");
-                    charset = charset.replaceFirst(" .*", "");
-                    
-                    if (! charset.equals(m_Charset))
+                    if (m_Charset != null)
                     {
-                        throw new IllegalStateException("Table " + name + " " +
-                            "has wrong character set " + charset + " " +
-                            "instead of " + m_Charset);
+	                    stmt = conn.prepareStatement("SHOW CREATE TABLE " +
+	                        this.m_Name);
+	                    rs = stmt.executeQuery();
+	                    rs.next();
+	                    String charset = rs.getString(2);
+	                    rs.close();
+	                    stmt.close();
+	                    
+	                    charset = charset.replaceFirst("(?s).* DEFAULT " +
+	                    		"CHARSET=", "");
+	                    charset = charset.replaceFirst(" .*", "");
+	                    
+	                    if (! charset.equals(m_Charset))
+	                    {
+	                    	throw new IllegalStateException("Table " + m_Name + 
+	                    			" has wrong character set " + charset + 
+	                    			" instead of " + m_Charset);
+	                    }
                     }
                     
                     if (!tci.hasNext())
@@ -150,7 +229,7 @@ public class DbTable
                         while (tci.hasNext()) 
                         {
                             DbColumn missing = (DbColumn)(tci.next());
-                            sql = "ALTER TABLE "+name+" ADD COLUMN "+
+                            sql = "ALTER TABLE "+m_Name+" ADD COLUMN "+
                             missing.getSpec();
                             stmt = conn.prepareStatement(sql);
                             stmt.executeUpdate();
@@ -160,7 +239,7 @@ public class DbTable
                     {
                         DbColumn missing = (DbColumn)(tci.next());
                         throw new IllegalStateException
-                            ("Table "+name+" missing column "+missing.name+
+                            ("Table "+m_Name+" missing column "+missing.name+
                             " " + missing.getSpec());
                     }
                     
@@ -168,12 +247,13 @@ public class DbTable
 				}
 			}
 			
-			sql = "CREATE TABLE "+name+ " (";
+			sql = "CREATE TABLE "+m_Name+ " (";
 			
-			for (int i = 0; i < columns.length; i++) {
-				DbColumn col = columns[i];
+			for (int i = 0; i < m_Columns.length; i++)
+			{
+				DbColumn col = m_Columns[i];
 				sql += col.getSpec();
-				if (i < columns.length - 1)
+				if (i < m_Columns.length - 1)
 					sql += ", ";
 			}
 			
